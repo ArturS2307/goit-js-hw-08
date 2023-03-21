cb25252fcce0effcf2a1146cc25dbd7d62c6561c
ArturS2307 <art.salakhutdinov@gmail.com>
2023-03-21 22:22:34 +0200

    gallery done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+console.log(galleryItems);
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const markup = createGallery(galleryItems);
+galleryContainer.insertAdjacentHTML('afterbegin', markup);
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+        </div>
+      `;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250, });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from "@vimeo/player";
+      const iframe = document.querySelector('iframe');
+      const player = new Player(iframe);
+  
+      player.on('play', function() {
+          console.log('played the video!');
+      });
+  
+      player.getVideoTitle().then(function(title) {
+          console.log('title:', title);
+      });
 
+    //   const timeupdate = function() {
+    //     console.log(timeupdate.duration);
+    //   }
+
+    //   player.on('timeupdate', timeupdate);
